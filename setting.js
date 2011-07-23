fb48194ad0ea0a70b812c8f2f1c9fc2295b38915
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-23 13:36:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/email-newsletter/trunk@414191 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ setting.js
@@ -0,0 +1,62 @@
+
+function eemail_submit()
+{
+	if(document.form_eemail.eemail_subject.value=="")
+	{
+		alert("Please enter the email subject.")
+		document.form_eemail.eemail_subject.focus();
+		return false;
+	}
+	else if(document.form_eemail.eemail_content.value=="")
+	{
+		alert("Please enter the email body.")
+		return false;
+	}
+	else if(document.form_eemail.eemail_status.value=="")
+	{
+		alert("Please select the display status.")
+		document.form_eemail.eemail_status.focus();
+		return false;
+	}
+}
+
+function _eemail_delete(id)
+{
+	if(confirm("Do you want to delete this record?"))
+	{
+		document.frm_eemail_display.action="options-general.php?page=email-newsletter/email-compose.php&AC=DEL&DID="+id;
+		document.frm_eemail_display.submit();
+	}
+}	
+
+function send_email_submit()
+{
+	if(document.form_eemail.eemail_subject_drop.value=="")
+	{
+		alert("Please select the email subject.")
+		return false;
+	}
+}
+
+function _eemail_redirect()
+{
+	window.location = "options-general.php?page=email-newsletter/email-compose.php";
+}
+
+
+function SetAllCheckBoxes(FormName, FieldName, CheckValue)
+{
+	
+	if(!document.forms[FormName])
+		return;
+	var objCheckBoxes = document.forms[FormName].elements[FieldName];
+	if(!objCheckBoxes)
+		return;
+	var countCheckBoxes = objCheckBoxes.length;
+	if(!countCheckBoxes)
+		objCheckBoxes.checked = CheckValue;
+	else
+		// set the check value for all check boxes
+		for(var i = 0; i < countCheckBoxes; i++)
+			objCheckBoxes[i].checked = CheckValue;
+}
