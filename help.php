7272d69a4ba2c562635c86a83307d1c393f07ab7
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-20 13:30:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/email-newsletter/tags/3.0@426317 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,46 @@
+<p>Sometimes you want an easy way to e-mail all the people who  registered, commented on the website, now it's as easy as installing this plug-in, </p>
+<p>All the instructions for installation, the support forums, a  FAQ, etc. can be found on the <a href="http://www.gopiplus.com/work/2010/09/25/email-newsletter/">plugin  home</a> page.</p>
+<h2>About Plugin</h2>
+Plug-in created by <a target="_blank" href='http://www.gopiplus.com/work/'>Gopi</a>.<br> 
+<a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.<br> 
+<a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to post suggestion & comments.<br> 
+<a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to download my other plugins.  
+<h2>Advantage</h2>
+<ul>
+  <li>Simple.</li>
+  <li>Easy installation.</li>
+  <li>Using this plug-in we can send email to all registered folks.</li>
+  <li>Using this plug-in we can send email to all comment posted folks.</li>
+  <li>Check box option available to select/unselect email from the list.</li>
+</ul>
+<h2>FAQ</h2> 
+1. How/Where to compose email?<br />		
+2. How to send email?<br />	
+3. How to send email to registered user?<br />		
+4. How to send email to comment posted user?<br />		
+5. How/Where to uncheck the particular user email from the list?<br />		
+5. Can i send the newsletter?<br />	
+6. How to design the newsletter?<br />
+<a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read answer.
+<h2>This plug-in broadly consider four menu</h2> 
+<h3>Plug-in Home</h3>
+<p>This page is to for help; in the page you can find the steps  and procedure for this plug-in, <a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</p>
+<h3>Compose email</h3>
+<p>This is the first step for this plug-in, before sending mail  to user, first compose the mail using this page, once you created the mail from  this page, the created mail displayed automatically in the send mail page, <a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</p>
+<h3>Send mail to registered user</h3>
+<p>This page is to send mail to all registered user, in this  page you can find the all registered user with check box option, if you don&rsquo;t want  to send any mail to particular user, you can uncheck the email from the list, after  you selected the users from the list select the email subject and click send  mail button, <a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</p>
+<h3>Send mail to comment posted user</h3>
+<p>This page is to send mail to all comment posted user, in  this page you can find all the comment posted user with check box option, if  you don&rsquo;t want to send any mail to particular user, you can uncheck the email  from the list, after you selected the users from the list select the email  subject and click send mail button, <a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</p>
+<h3>Donation</h3>
+<p>This plug-in is available under the GPL license, which means  that it's free. If you use it for a commercial web site, if you appreciate my efforts  or if you want to encourage me to develop and maintain it, please consider  making a donation using Papal, a secured payment solution. You just need to  click the donate button on the &nbsp;<a href="http://www.gopiplus.com/work/2010/09/25/email-newsletter/">plug-in home page</a> and follow the instructions.</p>
+<table width="100%">
+	<tr>
+	  <td align="center">
+		<input name="btn_plugin_home" lang="btn_plugin_home" class="button-primary" onClick="location.href='options-general.php?page=email-newsletter/email-newsletter.php'" value="Plugin home" type="button" />
+		<input name="btn_compose_email_page" lang="btn_compose_email_page" class="button-primary" onClick="location.href='options-general.php?page=email-newsletter/email-compose.php'" value="Compose email" type="button" />
+		<input name="btn_send_email_registered_user" lang="btn_send_email_registered_user" class="button-primary" onClick="location.href='options-general.php?page=email-newsletter/email-to-registered-user.php'" value="Send email to registered user" type="button" />
+		<input name="btn_send_email_comment_posted_user" lang="btn_send_email_comment_posted_user" class="button-primary" onClick="location.href='options-general.php?page=email-newsletter/email-to-comment-posed-user.php'" value="Send email to comment posed user" type="button" />
+	  </td>
+	</tr>
+</table>
+<br>
