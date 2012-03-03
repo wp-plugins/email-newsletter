69c9e2f1cf82187f470f044f1c47a5eb52dde680
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-03 15:47:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/email-newsletter/tags/5.0@513956 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/help.php
@@ -0,0 +1,69 @@
+<p>Sometimes you want an easy way to e-mail all the people who  registered, commented on the website, now it's as easy as installing this plug-in, </p>
+<p>All the instructions for installation, the support forums, a  FAQ, etc. can be found on the <a href="http://www.gopiplus.com/work/2010/09/25/email-newsletter/">plugin home</a> page.</p>
+
+<h2>About Plugin</h2>
+<ul>
+<li>Plug-in created by <a target="_blank" href='http://www.gopiplus.com/work/'>Gopi</a>.</li> 
+<li><a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</li>
+<li><a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to post suggestion & comments.</li>
+<li><a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to download my other plugins.</li>
+</ul>
+
+<h2>Advantage</h2><br>
+<ul>
+  <li>Simple.</li>
+  <li>Easy installation.</li>
+  <li>Using this plug-in we can send email to all registered folks.</li>
+  <li>Using this plug-in we can send email to all comment posted folks.</li>
+  <li>Check box option available to select/unselect email from the list.</li>
+  <li>Email subscribe form available for front end.</li>
+  <li>We can send email to all subscribed user.</li>
+  <li>Integrated the email newsletter wordpress plugin & simple contact form wordpress plugin.</li>
+</ul>
+
+<h2>This plug-in broadly consider five option</h2> <br>
+
+<strong>Compose email</strong> 
+
+<p>This is the first step for this plug-in, before sending mail to user, first we have to compose the mail using this page, once you created the mail content from this page, the created mail subject displayed automatically in the send mail page. <a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</p>
+
+<strong>Send mail to registered user</strong> 
+
+<p>This page is to send mail to all registered user, in this page you can find the all registered users email with check box option, if you don't want to send mail to any particular user(s), you can uncheck the email(s) from the list, after you selected the users from the list select the email subject and click send mail button. <a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</p>
+
+<strong>Send mail to comment posted user</strong> 
+
+<p>This page is to send mail to all comment posted user, in this page you can find all the comment posted user(s) email with check box option, if you don't want to send mail to any particular user(s), you can uncheck the email from the list, after you selected the users from the list select the email subject and click send mail button. <a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</p>
+
+<strong>Send mail to subscribed user</strong> 
+
+<p>From version 4.0 we have new option to subscribe the emails. After activated the plugin drag and drop the email newsletter widget to your sidebar, this will create the email subscribe form in the front end. With this form user can subscribe the email(s) to our newsletter.</p>
+
+<p>In the admin we have link "Send mail to subscribed user"; From this menu we can send email to subscribed user(s). In this page admin can see all the subscribed users email id with check-box option. Admin can check/un-check the user and can send the email to subscribed user(s). <a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</p>
+
+<p>In the "View subscribed user" admin menu. we can see all the subscribed user email list.</p>
+
+<strong>Send mail to simple contact form emails</strong> 
+
+<p>This is the new option. In my plugin list you can find the "simple contact form" WordPress plugin. Simple contact form just store the entered details in the admin to view, with the help of this email newsletter plugin you can send the email to those contact list. <a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</p>
+
+<p>Simply saying; i have integrated the email newsletter wordpress plugin & simple contact form wordpress plugin into this page. <a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</p>
+
+<p>Download and set up the simple contact form wordpress plugin to use this feature. If you don't want simple contact form; Simply omit this menu. <a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read more help.</p>
+
+<h2>Donation</h2>
+
+<p>This plug-in is available under the GPL license, which means  that it's free. If you use it for a commercial web site, if you appreciate my efforts or if you want to encourage me to develop and maintain it, please consider  making a donation using Papal, a secured payment solution. You just need to click the donate button on the &nbsp;<a href="http://www.gopiplus.com/work/2010/09/25/email-newsletter/">plug-in home page</a> and follow the instructions.</p>
+
+<p>If you want to remove this dotation topic from the page, go and check the help.php file and remove it.</p>
+
+<h2>FAQ</h2> 
+1. How/Where to compose email?<br />		
+2. How to send email?<br />	
+3. How to send email to registered user?<br />		
+4. How to send email to comment posted user?<br />		
+5. How/Where to uncheck the particular user email from the list?<br />		
+5. Can i send the HTML newsletter?<br />	
+6. How to design the newsletter?<br />
+6. How to add email subscrib form in the front end?<br />
+<a target="_blank" href='http://www.gopiplus.com/work/2010/09/25/email-newsletter/'>Click here</a> to read answer.<br /><br />
