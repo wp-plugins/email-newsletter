7f326b95a2171b966d03eb0a1ec9843d7128c791
tanaylakhani <tanaylakhani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-10 16:21:50 +0000

    bug fix
    git-svn-id: https://plugins.svn.wordpress.org/email-newsletter/trunk@1178362 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -6,7 +6,7 @@ Plugin URI: http://www.gopiplus.com/work/2010/09/25/email-newsletter/
 Tags: Email, Newsletter, SendMail, Contact, plugin, widgets
 Requires at least: 3.0
 Tested up to: 4.1
-Stable tag: 20.13.1
+Stable tag: 20.13.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 	
@@ -610,6 +610,10 @@ Subsequent upgrades wont load readygraph except a plugin is disabled and re-enab
 
 Bug Fix for readygraph load
 
+= 20.13.2 =
+
+Bug Fix for readygraph
+
 == Upgrade Notice ==
 
 = 1.0 =
@@ -809,3 +813,7 @@ Subsequent upgrades wont load readygraph except a plugin is disabled and re-enab
 = 20.13.1 =
 
 Bug Fix for readygraph load
+
+= 20.13.2 =
+
+Bug Fix for readygraph
--- widget/widget.php
+++ widget/widget.php
@@ -11,7 +11,9 @@
     <input class="eemail_textbox_class" name="eemail_txt_email" id="eemail_txt_email" onkeypress="if(event.keyCode==13) eemail_submit_ajax('<?php echo emailnews_plugin_url('widget'); ?>')" onblur="if(this.value=='') this.value='<?php echo get_option('eemail_widget_txt_cap'); ?>';" onfocus="if(this.value=='<?php echo get_option('eemail_widget_txt_cap'); ?>') this.value='';" value="<?php echo get_option('eemail_widget_txt_cap'); ?>" maxlength="150" type="text">
   </div>
   <?php 
-  if(get_option('readygraph_application_id') && strlen(get_option('readygraph_application_id')) > 0){?>
+	$readygraph_api = get_option('readygraph_application_id');
+	$readygraph_access_token = get_option('readygraph_access_token');
+	if($readygraph_api && strlen($readygraph_api) > 0 && $readygraph_access_token && strlen($readygraph_access_token) > 0){?>
   <p style="max-width:180px;font-size: 10px;margin-bottom:10px;">By signing up, you agree to our <a href="http://www.readygraph.com/tos">Terms of Service</a> and <a href='http://readygraph.com/privacy/'>Privacy Policy</a>.</p>
   <?php } ?>
   <div class="eemail_button">
